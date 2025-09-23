aca3c77ec9362815189e54920718dcfcc6648895
Isabella Schepisi <schepisii2@gmail.com>
2025-09-23 14:58:18 -0500

    Use vue full address

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
